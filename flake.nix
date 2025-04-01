d796a288362dd3c2d0ac328ae72a9b957c84ecbe
malev <marcosvanetta@gmail.com>
2025-04-01 17:06:48 -0500

    Adding a flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "hola.nvim's flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        hola-nvim = pkgs.vimUtils.buildVimPlugin {
+          pname = "hola-nvim";
+          version = self.shortRev or "dirty";
+          src = self;
+          doCheck = false;
+        };
+        plenaryPlugin = pkgs.vimPlugins.plenary-nvim;
+      in
+      {
+        packages = {
+          ${hola-nvim.pname} = hola-nvim;
+          plenary = plenaryPlugin;
+          default = hola-nvim;
+        };
+        defaultPackage = self.packages.${system}.default;
+        nvimPlugins = {
+          ${hola-nvim} = hola-nvim;
+          plenary-nvim = plenaryPlugin;
+        };
+      });
+}
